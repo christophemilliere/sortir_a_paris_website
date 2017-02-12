18a37c11e2ca377b6697153e5a2cdaa00665f8ad
christophemilliere <christophemilliere93@gmail.com>
2017-02-12 21:15:29 +0100

    get one monument

--- app/api/sap/v1/monuments.rb
+++ app/api/sap/v1/monuments.rb
@@ -5,6 +5,19 @@ module V1
       get  do
         Monument.all
       end
+
+      desc " Return one monument"
+      params do
+        requires :monument_id
+      end
+      get '/:monument_id' do
+        monument = Monument.find_by_id(params[:monument_id])
+        if monument
+          monument
+        else
+           error!("Aucun monument trouvé" , 404)
+        end
+      end
     end
   end
 end
